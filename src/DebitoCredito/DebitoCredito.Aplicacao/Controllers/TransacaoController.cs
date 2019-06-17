28750fff59f905f99791b3156259ce3ef80ffd65
Danilo de Oliveira <doliveira.doti@bancomaxima.com.br>
2019-06-17 19:46:27 -0300

    Configuração do ambiente de teste

---
+++ src/DebitoCredito/DebitoCredito.Aplicacao/Controllers/TransacaoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebitoCredito.Aplicacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacaoController : ControllerBase
+    {
+        private readonly string _idRequest;
+
+        public TransacaoController()
+        {
+            _idRequest = Guid.NewGuid().ToString();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Response.Headers.Add("id-request", _idRequest);
+        }
+    }
+}
---
+++ src/DebitoCredito/DebitoCredito.Teste/Configs/TestContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using DebitoCredito.Aplicacao;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace DebitoCredito.Teste.Configs
+{
+    public class TestContext
+    {
+        public HttpClient Client { get; set; }
+        private TestServer _server;
+
+        public TestContext()
+        {
+            SetupClient();
+        }
+
+        private void SetupClient()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = _server.CreateClient();
+        }
+    }
+}
