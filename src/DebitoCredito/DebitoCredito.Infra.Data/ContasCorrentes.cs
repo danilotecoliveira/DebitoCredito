08263151d3074f330bd20753c49a210830cd39e8
Danilo de Oliveira <doliveira.doti@bancomaxima.com.br>
2019-06-17 23:00:22 -0300

    Configurando o repositório e escrevendo os testes unitários

--- src/DebitoCredito/DebitoCredito.Infra.Data/ContasCorrentes.cs
+++ src/DebitoCredito/DebitoCredito.Infra.Data/ContasCorrentes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
 using DebitoCredito.Dominio.Interfaces.Infra;
 
 namespace DebitoCredito.Infra.Data
@@ -7,7 +9,17 @@ namespace DebitoCredito.Infra.Data
     {
         public bool ValidarContaCadastrada(string contaCorrente)
         {
-            throw new NotImplementedException();
+            using (var conn = new SqlConnection(VariaveisGlobais.Conn))
+            {
+                var param = new
+                {
+                    CONTACORRENTE = contaCorrente
+                };
+
+                var resultado = conn.QueryFirstOrDefault<int>("SELECT COUNT(NUMERO) FROM CONTASCORRENTES WHERE NUMERO = @CONTACORRENTE", param, commandType: CommandType.Text);
+
+                return resultado == 1;
+            }
         }
     }
 }
---
+++ src/DebitoCredito/DebitoCredito.Teste/Infra/ContasCorrentesTeste.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using DebitoCredito.Infra.Data;
+using DebitoCredito.Dominio.Interfaces.Infra;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DebitoCredito.Teste.Infra
+{
+    public class ContasCorrentesTeste
+    {
+        private readonly IContasCorrentes _contasCorrentes;
+
+        public ContasCorrentesTeste()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IContasCorrentes, ContasCorrentes>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            _contasCorrentes = serviceProvider.GetService<IContasCorrentes>();
+        }
+
+        [Theory]
+        [InlineData("0123", true)]
+        [InlineData("01234", false)]
+        public void Testar_Validar_Conta_Cadastrada(string contaCorrente, bool esperado)
+        {
+            var resultado = _contasCorrentes.ValidarContaCadastrada(contaCorrente);
+
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
